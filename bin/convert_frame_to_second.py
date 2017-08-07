ad9ee20f05af6f5d88b601784a936cc3bd1c3693
doridoridoriand <i.am.eager.for.peace@gmail.com>
2017-08-08 01:16:53 +0900

    fix typo

--- bin/convert_frame_to_second.py
+++ bin/convert_frame_to_second.py
@@ -6,7 +6,7 @@ for content in contents:
         frame = content.split(' ')[2][9:]
         seconds = str(int(int(frame) / 25))
         source_url = 'https://www.youtube.com/watch?v=fYB1Ovpd0nc&t='
-        urls.append(source_url + seconds + 't')
+        urls.append(source_url + seconds + 's')
 
 for url in sorted(set(urls)):
     print(url)
