1875dcaae11305ae45753cf53f144915d1a77d37
doridoridoriand <i.am.eager.for.peace@gmail.com>
2017-08-08 00:52:51 +0900

    add convert frame to second

---
+++ bin/convert_frame_to_second.py
@@ -0,0 +1,12 @@
+contents = open('result.log').readlines()
+
+urls = []
+for content in contents:
+    if content.replace('\n', '').split(' ')[-1] == '5':
+        frame = content.split(' ')[2][9:]
+        seconds = str(int(int(frame) / 25))
+        source_url = 'https://www.youtube.com/watch?v=fYB1Ovpd0nc&t='
+        urls.append(source_url + seconds + 't')
+
+for url in sorted(set(urls)):
+    print(url)
